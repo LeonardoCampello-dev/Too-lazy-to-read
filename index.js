7fa694a494705d701ebd271ae14f02c747250472
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-10-25 20:56:38 -0300

    Initial script

---
+++ index.js
@@ -0,0 +1,15 @@
+const uterance = new SpeechSynthesisUtterance()
+
+uterance.lang = 'pt-BR'
+
+function speak() {
+    speechSynthesis.speak(uterance)
+}
+
+function stop() {
+    speechSynthesis.cancel()
+}
+
+function setText(event) {
+    uterance.text = event.target.innerText
+}
