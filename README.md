a47992306857c3da62fe59e694309aefe3d36c43
Leonardo Campello <leonardocampello.dev@gmail.com>
2020-10-26 18:38:46 -0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+![image](lazy.png)
+
+## 🚀 Technologies used
+
+- [x] [Bulma](https://bulma.io/)
+- [x] [SpeechSynthesis](https://developer.mozilla.org/pt-BR/docs/Web/API/SpeechSynthesis)
--- index.js
+++ index.js
@@ -10,7 +10,15 @@ function stop() {
     speechSynthesis.cancel()
 }
 
+function clear() {
+    document.querySelector('.text-box').innerText = ''
+}
+
 function setText(event) {
     uterance.text = event.target.innerText
+}
+
+function setLang(event) {
+    uterance.lang = event.target.innerText
 }
 
